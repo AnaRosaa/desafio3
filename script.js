be20ec9231a6f97bb59d302ae5dce37a096217d8
anarosaa <silvaanarosa103@gmail.com>
2022-06-15 18:51:01 -0300

    Desafio while/for

---
+++ script.js
@@ -0,0 +1,32 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for(let uneTen=1; uneTen<=10; uneTen++){
+    console.log(uneTen);
+}
+
+
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+let tenTwo=10
+while(1<= tenTwo){
+    console.log(tenTwo);
+    tenTwo=1
+}
+    
+
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for(let hundredOne=1; hundredOne<=100; hundredOne+=2){
+    console.log(hundredOne);
+}
+
+
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+for(let hundredTwo=0; hundredTwo<=100; hundredTwo+=2){
+    console.log(hundredTwo);
+}
+
